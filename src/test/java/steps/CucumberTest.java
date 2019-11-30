96db5a35c28dde0ea186f509f714641b52a9d0f5
wyf <ecwyf@126.com>
2019-11-30 19:25:41 +0800

    cucumber report json

--- src/test/java/steps/CucumberTest.java
+++ src/test/java/steps/CucumberTest.java
@@ -5,7 +5,7 @@ import cucumber.api.junit.Cucumber;
 import org.junit.runner.RunWith;
 
 @RunWith(Cucumber.class)
-@CucumberOptions(features = {"src/test/resources/features/"})
+@CucumberOptions(features = {"src/test/resources/features/"}, format = "json:target/cucumber/cucumber.json")
 public class CucumberTest {
 }
 
